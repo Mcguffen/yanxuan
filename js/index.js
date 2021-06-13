15c60efc567c40d5a3c3a20681ed20bc5f34c381
Mcguffen <504177380@qq.com>
2021-06-13 14:46:48 +0800

    轮播图组件完成

--- js/index.js
+++ js/index.js
@@ -3,3 +3,28 @@ yx.public.navFn();
 yx.public.lazyImgFn();
 yx.public.backUpFn()
 
+//banner图轮播
+var bannerPic=new Carousel();
+bannerPic.init({
+	id:'bannerPic',
+	autoplay:true,
+	intervalTime:3000,
+	loop:true,
+	totalNum:5,
+	moveNum:1,
+	circle:true,
+	moveWay:'opacity'
+});
+
+//新品首发轮播图
+var newProduct=new Carousel();
+newProduct.init({
+	id:'newProduct',
+	autoplay:false,
+	intervalTime:3000,
+	loop:false,
+	totalNum:8,
+	moveNum:4,
+	circle:false,
+	moveWay:'position'
+});
