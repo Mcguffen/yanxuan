401bd28fcce863f6ac277a613d5f8f28f0d6d263
Mcguffen <504177380@qq.com>
2021-06-13 21:00:16 +0800

    加减功能和产品图片切换功能

--- js/product.js
+++ js/product.js
@@ -15,31 +15,71 @@ if(!pageId || !curData){
 	window.location.href='404.html';
 }
 
-//创建规格DOM
-var format=yx.g('#productImg .fomat');
-var dds=[];			//把所有的dd标签存起来，为了后面要用
-for(var i=0;i<curData.skuSpecList.length;i++){
-	var dl=document.createElement("dl");
-	var dt=document.createElement("dt");
-	dt.innerHTML=curData.skuSpecList[i].name;
-	dl.appendChild(dt);
+//面包屑的功能
+var positionFn=yx.g('#position');
+positionFn.innerHTML='<a href="#">首页</a> >';
+for(var i=0;i<curData.categoryList.length;i++){
+	positionFn.innerHTML+=' <a href="#">'+curData.categoryList[i].name+'</a> >';
+}
+positionFn.innerHTML+=curData.name;
+//http://www.kaivon.cn/getData.php?id=1131017
+
+
+//产品图功能
+(function(){
+	//左边图片切换功能
+	var bigImg=yx.g('#productImg .left img');
+	var smallImgs=yx.ga('#productImg .smallImg img');
 	
-	for(var j=0;j<curData.skuSpecList[i].skuSpecValueList.length;j++){
-		var dd=document.createElement("dd");
-		dd.innerHTML=curData.skuSpecList[i].skuSpecValueList[j].value;
-		dd.setAttribute('data-id',curData.skuSpecList[i].skuSpecValueList[j].id)
+	bigImg.src=smallImgs[0].src=curData.primaryPicUrl;
+	
+	var last=smallImgs[0];		//上一张图片
+	for(var i=0;i<smallImgs.length;i++){
+		if(i){
+			//这个条件满足的话，说明现在是后四张图片
+			smallImgs[i].src=curData.itemDetail['picUrl'+i];
+		}
 		
-		dd.onclick=function(){
-			changeProduct.call(this);
+		smallImgs[i].index=i;
+		smallImgs[i].onmouseover=function(){
+			bigImg.src=this.src;
+			last.className='';
+			this.className='active';
+			
+			last=this;
 		};
-		
-		dds.push(dd);
-		dl.appendChild(dd);
 	}
 	
-	format.appendChild(dl);
-}
-
+	//右边图片相关信息更换
+	yx.g('#productImg .info h2').innerHTML=curData.name;
+	yx.g('#productImg .info p').innerHTML=curData.simpleDesc;
+	yx.g('#productImg .info .price').innerHTML='<div><span>售价</span><strong>¥'+curData.retailPrice+'.00</strong></div><div><span>促销</span><a href="'+curData.hdrkDetailVOList[0].huodongUrlPc+'" class="tag">'+curData.hdrkDetailVOList[0].activityType+'</a><a href="'+curData.hdrkDetailVOList[0].huodongUrlPc+'" class="discount">'+curData.hdrkDetailVOList[0].name+'</a></div><div><span>服务</span><a href="#" class="service"><i></i>30天无忧退货<i></i>48小时快速退款<i></i>满88元免邮费<i></i>网易自营品牌</a></div>';
+	
+	//创建规格DOM
+	var format=yx.g('#productImg .fomat');
+	var dds=[];			//把所有的dd标签存起来，为了后面要用
+	for(var i=0;i<curData.skuSpecList.length;i++){
+		var dl=document.createElement("dl");
+		var dt=document.createElement("dt");
+		dt.innerHTML=curData.skuSpecList[i].name;
+		dl.appendChild(dt);
+		
+		for(var j=0;j<curData.skuSpecList[i].skuSpecValueList.length;j++){
+			var dd=document.createElement("dd");
+			dd.innerHTML=curData.skuSpecList[i].skuSpecValueList[j].value;
+			dd.setAttribute('data-id',curData.skuSpecList[i].skuSpecValueList[j].id)
+			
+			dd.onclick=function(){
+				changeProduct.call(this);
+			};
+			
+			dds.push(dd);
+			dl.appendChild(dd);
+		}
+		
+		format.appendChild(dl);
+	}
+	
 	//点击规格功能
 	function changeProduct(){
 		//如果不能点击的话就返回
@@ -72,52 +112,102 @@ for(var i=0;i<curData.skuSpecList.length;i++){
 		}
 		
 		
-	//点击的功能
-	/*
-	 * 点击的时候判断
-	 * 	1、自己是末选中状态
-	 * 		1、兄弟节点
-	 * 			有选中的话要取消选中，有不能点击的不用处理
-	 * 		2、自己选中
-	 * 		3、对方节点
-	 * 			先去掉有noclick的class的元素，再给不能点击的加上noclick
-	 * 		
-	 * 	2、自己是选中状态
-	 * 		1、取消自己选中
-	 * 			（兄弟节点不用处理）
-	 * 		2、对方节点
-	 * 			如果有不能点击的要去掉noclick的class
-	 */
-	
-	var brothers=this.parentNode.querySelectorAll('dd');		//兄弟节点
-	if(this.className=='active'){
-		//选中状态
-		this.className='';
+		//点击的功能
+		/*
+		 * 点击的时候判断
+		 * 	1、自己是末选中状态
+		 * 		1、兄弟节点
+		 * 			有选中的话要取消选中，有不能点击的不用处理
+		 * 		2、自己选中
+		 * 		3、对方节点
+		 * 			先去掉有noclick的class的元素，再给不能点击的加上noclick
+		 * 		
+		 * 	2、自己是选中状态
+		 * 		1、取消自己选中
+		 * 			（兄弟节点不用处理）
+		 * 		2、对方节点
+		 * 			如果有不能点击的要去掉noclick的class
+		 */
 		
-		for(var i=0;i<othersDd.length;i++){
-			if(othersDd[i].className=='noclick'){
-				othersDd[i].className='';
+		var brothers=this.parentNode.querySelectorAll('dd');		//兄弟节点
+		if(this.className=='active'){
+			//选中状态
+			this.className='';
+			
+			for(var i=0;i<othersDd.length;i++){
+				if(othersDd[i].className=='noclick'){
+					othersDd[i].className='';
+				}
+			}
+		}else{
+			//末选中状态
+			for(var i=0;i<brothers.length;i++){
+				if(brothers[i].className=='active'){
+					brothers[i].className='';
+				}
+			}
+			
+			this.className='active';
+			
+			for(var i=0;i<othersDd.length;i++){
+				if(othersDd[i].className=='noclick'){
+					othersDd[i].className='';
+				}
+				if(curData.skuMap[mergeId[i]].sellVolume==0){
+					othersDd[i].className='noclick';
+				}
 			}
 		}
-	}else{
-		//末选中状态
-		for(var i=0;i<brothers.length;i++){
-			if(brothers[i].className=='active'){
-				brothers[i].className='';
-			}
-		}
 		
-		this.className='active';
-		
-		for(var i=0;i<othersDd.length;i++){
-			if(othersDd[i].className=='noclick'){
-				othersDd[i].className='';
-			}
-			if(curData.skuMap[mergeId[i]].sellVolume==0){
-				othersDd[i].className='noclick';
-			}
-		}
+		addNum();
 	}
 	
+	//加减功能
 	addNum();
-}
+	function addNum(){
+		var actives=yx.ga('#productImg .fomat .active');
+		var btnParent=yx.g('#productImg .number div');
+		var btns=btnParent.children;
+		var ln=curData.skuSpecList.length;		//规格的数量
+		
+		//是否打开加减功能
+		//console.log(actives.length,ln)
+		if(actives.length==ln){
+			//这个条件成立说明用户选中所有的规格了，这个时候就要把加减的功能打开
+			btnParent.className='';
+		}else{
+			btnParent.className='noClick';
+		}
+		
+		//减号点击
+		btns[0].onclick=function(){
+			if(btnParent.className){
+				return;
+			}
+			
+			btns[1].value--;
+			if(btns[1].value<0){
+				btns[1].value=0;
+			}
+		};
+		
+		//input点击
+		btns[1].onfocus=function(){
+			if(btnParent.className){
+				//如果父级是不能点击的时候，就要让输入框永远失去焦点
+				this.blur();
+			}
+		};
+		
+		//加号点击
+		btns[2].onclick=function(){
+			if(btnParent.className){
+				return;
+			}
+			
+			btns[1].value++;
+		};
+	}
+})();
+
+
